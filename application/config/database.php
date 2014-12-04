e239e65906853be22d759aaf5c8b69b3560c9722
Jason Tordsen <jason@Jasons-MacBook-Pro.local>
2014-12-04 10:08:13 -0500

    database username change

--- application/config/database.php
+++ application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = 'root';
+$db['default']['username'] = 'holidaycard-2014';
+$db['default']['password'] = 'holidaycard-2014';
 $db['default']['database'] = 'holidaycard-2014';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
