4da364872811026b777cb646d0bfca43e7878e17
Jason Tordsen <jason@jasons-mbp.home>
2014-12-14 13:50:55 -0500

    removing test code

--- scripts/pages/tshirt.js
+++ scripts/pages/tshirt.js
@@ -19,7 +19,7 @@ define([
                 this.$el.html( this.template( this.session.attributes.client.attributes ) );
             }else{
                 this.$el.html( "<h5>Sorry, that's not a valid gift code.</h5>");
-                this.$el.html( this.template( {id:"0", first_name:"Jason", address:"15 Warren St #121", city:"Jersey City", state:"NJ", zip:"07302"} ) );
+                //this.$el.html( this.template( {id:"0", first_name:"Jason", address:"15 Warren St #121", city:"Jersey City", state:"NJ", zip:"07302"} ) );
             }
         },
         activate:function(){
